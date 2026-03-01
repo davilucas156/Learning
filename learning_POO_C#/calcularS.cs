104f5cee0b8da103c1f94fa2ce98e265c1e75e84
davilucas156 <142557764+davilucas156@users.noreply.github.com>
2026-03-01 17:41:57 -0300

    função recursiva para calcular uma série

---
+++ learning_POO_C#/calcularS.cs
@@ -0,0 +1,23 @@
+// using System; 
+
+// class Program{
+//     static double serieHarmonica(int n)
+//     {
+//         if (n <= 1)
+//         return 1.0;
+
+//         else
+//          return (1.0 / n) + serieHarmonica(n - 1);
+
+//     }
+//     static void Main(string[] args)
+//     {
+//         int n = 0;
+
+//         Console.WriteLine("insira o valor para n");
+//         n = int.Parse(Console.ReadLine());
+
+//         Console.WriteLine("resultado é :" + serieHarmonica(n));
+//     }
+
+// }
