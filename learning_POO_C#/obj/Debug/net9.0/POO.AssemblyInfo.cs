32cf4f3372f08edc9f6663aa981c33d4497d4968
davilucas156 <142557764+davilucas156@users.noreply.github.com>
2026-03-02 19:52:21 -0300

    organizando pastas

--- learning_POO_C#/obj/Debug/net9.0/POO.AssemblyInfo.cs
+++ learning_POO_C#/obj/Debug/net9.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62fbac789bbdb6ec2992d0fae206b5955d14715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884b565fdf081c153f518f066a10d192c062e839")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
